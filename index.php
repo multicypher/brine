f185d33842a465e1e3345560b4e33dfbd1b6afcf
MULTICYPHER <multicypher@gmail.com>
2020-09-29 13:35:45 -0700

    header added

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
 <?php
+/**
+ * Main template file
+ *
+ * @package Brine
+ *
+ */
+
+get_header();
+?>
+
+<!doctype html>
+<html lang="<?php language_attributes(); ?>">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class();?>>
+
+<?php
+get_footer();
+
